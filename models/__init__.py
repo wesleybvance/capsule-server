c49ec4d910a45ee834c34cc1e086414bf7bb31bc
Wesley Vance <wslyvnce@gmail.com>
2023-08-21 19:44:48 -0500

    user fixture created

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .capsule_user import CapsuleUser
 from .category import Category
 from .item import Item
-from .outfit import Outfit
 from .outfit_item import OutfitItem
+from .outfit import Outfit
