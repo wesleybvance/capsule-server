b1b02513c127a2b6e39f82ebc210ecfcdbe21a11
Wesley Vance <wslyvnce@gmail.com>
2023-09-01 04:09:34 -0500

    mvp ready zzzz

--- capsuleapi/views/auth.py
+++ capsuleapi/views/auth.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from capsuleapi.models.capsule_user import CapsuleUser
-from rest_framework import status
 
 
 @api_view(['POST'])
